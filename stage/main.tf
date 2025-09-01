51b8f4fe590557107cf72aee4fcc68fcd3625688
Rutvik <rutvik@popclub.co>
2025-09-01 07:05:17 +0530

    added vpc and subnets for stage

---
+++ stage/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0 "
+    }
+  }
+  backend "s3" {
+    bucket = "tf-state-bucket-s3-2025"
+    key = "tf-infra/terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "terraform-state-locking-2025"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "tf_state_bucket_s3_2025" {
+  bucket        = "tf-state-bucket-s3-2025"
+  force_destroy = true
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking-2025"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
---
+++ terraform-imports/imports.tf
@@ -0,0 +1,25 @@
+import {
+  to = aws_subnet.SubnetPrivateAPSOUTH1B
+  id = "subnet-0e1b4d92c477697aa"
+}
+
+import {
+  to = aws_subnet.SubnetPublicAPSOUTH1B
+  id = "subnet-03102625621ec9e72"
+}
+
+import {
+  to = aws_subnet.SubnetPrivateAPSOUTH1A
+  id = "subnet-04a939c0eab88cf17"
+}
+
+import {
+  to = aws_subnet.SubnetPublicAPSOUTH1A
+  id = "subnet-0412aaae2878b5587"
+}
+
+
+
+
+
+
