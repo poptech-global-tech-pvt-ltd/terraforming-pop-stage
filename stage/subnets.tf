e556dd5e90a265c9ac522ababfcad84c4880ebd3
Rutvik <rutvik@popclub.co>
2025-09-05 21:40:19 +0530

    testing

--- stage/subnets.tf
+++ stage/subnets.tf
@@ -133,3 +133,36 @@ resource "aws_subnet" "SubnetPublicAPSOUTH1A" {
   }
   vpc_id = "vpc-0f63840cd962173c5"
 }
+resource "aws_subnet" "testing-subnet-creation" {
+  assign_ipv6_address_on_creation                = false
+  availability_zone                              = "ap-south-1a"
+  cidr_block                                     = "192.168.0.0/19"
+  customer_owned_ipv4_pool                       = null
+  enable_dns64                                   = false
+  # enable_lni_at_device_index                     = 0
+  enable_resource_name_dns_a_record_on_launch    = false
+  enable_resource_name_dns_aaaa_record_on_launch = false
+  ipv6_cidr_block                                = null
+  ipv6_native                                    = false
+  map_public_ip_on_launch                        = true
+  outpost_arn                                    = null
+  private_dns_hostname_type_on_launch            = "ip-name"
+  region                                         = "ap-south-1"
+  tags = {
+    Name                                          = "eksctl-pop-stage-cluster/SubnetPublicAPSOUTH1A"
+    "alpha.eksctl.io/cluster-name"                = "pop-stage"
+    "alpha.eksctl.io/cluster-oidc-enabled"        = "false"
+    "alpha.eksctl.io/eksctl-version"              = "0.167.0"
+    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "pop-stage"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+  tags_all = {
+    Name                                          = "eksctl-pop-stage-cluster/SubnetPublicAPSOUTH1A"
+    "alpha.eksctl.io/cluster-name"                = "pop-stage"
+    "alpha.eksctl.io/cluster-oidc-enabled"        = "false"
+    "alpha.eksctl.io/eksctl-version"              = "0.167.0"
+    "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "pop-stage"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+  vpc_id = "vpc-0f63840cd962173c5"
+}
