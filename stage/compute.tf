6f517b0aa67ea7eae535289809801601c40aca2e
Rutvik <rutvik@popclub.co>
2025-09-14 23:20:22 +0530

    new ec2

--- stage/compute.tf
+++ stage/compute.tf
@@ -1,8 +1,21 @@
+# module "ec2_instance" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+
+#   name = "actions-runner-stage"
+
+#   instance_type = "t3.micro"
+#   subnet_id     = "subnet-0412aaae2878b5587"
+
+#   tags = {
+#     env="stage"
+#     team="devops"
+#   }
+# }
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "actions-runner-stage"
-
+  name = "actions-runner-stage-2"
+  key_name = "actions-runner-key"
   instance_type = "t3.micro"
   subnet_id     = "subnet-0412aaae2878b5587"
 
