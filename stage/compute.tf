88c710eb6c50e9ecf6b6e4a877a34793e0e4e75d
Rutvik <rutvik@popclub.co>
2025-09-14 23:45:05 +0530

    using bastion sg

--- stage/compute.tf
+++ stage/compute.tf
@@ -5,6 +5,8 @@ module "ec2_instance_2" {
   key_name = "actions-runner-key"
   instance_type = "t3.micro"
   subnet_id     = "subnet-0412aaae2878b5587"
+  create_security_group = false
+  vpc_security_group_ids = ["sg-0d0ecce28af0d66c2"]
 
   tags = {
     env="stage"
